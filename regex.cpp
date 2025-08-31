1aa1b899ba03a55d039e5fa8e145e91e517a5d62
Muhammad Rehman <ammuhammadrehman@gmail.com>
2025-08-31 16:34:04 +0500

    implement lexer using regex

---
+++ regex.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <regex>
+#include "tokens.h"
+
+using namespace std;
+
+// Patterns for regex-based lexer
+vector<pair<TokenType, regex>> tokenPatterns = {
+    // Keywords
+    {TokenType::T_FUNCTION, regex("^fn\\b")},
+    {TokenType::T_INT, regex("^int\\b")},
+    {TokenType::T_FLOAT, regex("^float\\b")},
+    {TokenType::T_STRING, regex("^string\\b")},
+    {TokenType::T_BOOL, regex("^bool\\b")},
+    {TokenType::T_RETURN, regex("^return\\b")},
+    {TokenType::T_IF, regex("^if\\b")},
+    {TokenType::T_ELSE, regex("^else\\b")},
+    {TokenType::T_WHILE, regex("^while\\b")},
+    {TokenType::T_FOR, regex("^for\\b")},
+    {TokenType::T_BOOLLIT, regex("^(true|false)\\b")},
+
+    // Literals
+    {TokenType::T_FLOATLIT, regex("^[0-9]+\\.[0-9]+")},
+    {TokenType::T_INTLIT, regex("^[0-9]+")},
+    {TokenType::T_STRINGLIT, regex("^\"([^\"\\\\]|\\\\.)*\"")},
+
+    // Operators & Symbols
+    {TokenType::T_EQUALSOP, regex("^==")},
+    {TokenType::T_NOTEQUAL, regex("^!=")},
+    {TokenType::T_LESSEQ, regex("^<=")},
+    {TokenType::T_GREATEREQ, regex("^>=")},
+    {TokenType::T_ASSIGN, regex("^=")},
+    {TokenType::T_LESS, regex("^<")},
+    {TokenType::T_GREATER, regex("^>")},
+    {TokenType::T_PLUSASSIGN, regex("^\\+=")},
+    {TokenType::T_MINUSASSIGN, regex("^-=")},
+    {TokenType::T_PLUS, regex("^\\+")},
+    {TokenType::T_MINUS, regex("^-")},
+    {TokenType::T_MULT, regex("^\\*")},
+    {TokenType::T_DIV, regex("^/")},
+    {TokenType::T_MOD, regex("^%")},
+    {TokenType::T_AND, regex("^&&")},
+    {TokenType::T_OR, regex("^\\|\\|")},
+    {TokenType::T_NOT, regex("^!")},
+    {TokenType::T_BITAND, regex("^&")},
+    {TokenType::T_BITOR, regex("^\\|")},
+    {TokenType::T_BITXOR, regex("^\\^")},
+    {TokenType::T_BITNOT, regex("^~")},
+    {TokenType::T_LSHIFT, regex("^<<")},
+    {TokenType::T_RSHIFT, regex("^>>")},
+
+    // Punctuation
+    {TokenType::T_PARENL, regex("^\\(")},
+    {TokenType::T_PARENR, regex("^\\)")},
+    {TokenType::T_BRACEL, regex("^\\{")},
+    {TokenType::T_BRACER, regex("^\\}")},
+    {TokenType::T_BRACKETL, regex("^\\[")},
+    {TokenType::T_BRACKETR, regex("^\\]")},
+    {TokenType::T_COMMA, regex("^,")},
+    {TokenType::T_SEMICOLON, regex("^;")},
+    {TokenType::T_QUOTE, regex("^\"")},
+
+    // Identifiers (must be after keywords)
+    {TokenType::T_IDENTIFIER, regex("^[a-zA-Z_][a-zA-Z0-9_]*")},
+    
+    // Comments Handling
+    {TokenType::T_COMMENT, regex("^//[^\n]*")},
+    {TokenType::T_COMMENT, regex("^/\\*[^*]*\\*+([^/*][^*]*\\*+)*/")}
+    
+};
+
+
+vector<Token> tokenize_regex(const string &input) {
+    vector<Token> tokens;
+    string code = input;
+
+    while (!code.empty()) {
+        // Skip whitespace
+        if (isspace(code[0])) {
+            code.erase(0, 1);
+            continue;
+        }
+
+        bool matched = false;
+
+        for (auto &[type, pattern] : tokenPatterns) {
+            smatch match;
+            if (regex_search(code, match, pattern)) {
+                string value = match.str(0);
+                tokens.push_back(Token(type, value));
+                code.erase(0, value.size());
+                matched = true;
+                break;
+            }
+        }
+
+        if (!matched) {
+            // Unknown token
+            tokens.push_back(Token(TokenType::T_UNKNOWN, string(1, code[0])));
+            code.erase(0, 1);
+        }
+    }
+
+    tokens.push_back(Token(TokenType::T_EOF, ""));
+    return tokens;
+}
+
+
+void print_all_tokens(const vector<Token>& tokens) {
+    cout << "[";
+    for (auto &t : tokens) {
+        cout << fromTokenTypeToStringGo(t.type);
+        if (!t.value.empty() && (t.type == TokenType::T_STRINGLIT || t.type == TokenType::T_IDENTIFIER))
+            cout << "(\"" << t.value << "\")";
+        else if (!t.value.empty() && (t.type == TokenType::T_INTLIT || 
+                                      t.type == TokenType::T_FLOATLIT ||
+                                      t.type == TokenType::T_BOOLLIT))
+            cout << "(" << t.value << ")";
+        cout << ",";
+    }
+    cout << "]";
+    cout << endl;
+}
+
+int main() {
+    ifstream infile("TestFile.txt");
+    stringstream buffer;
+    buffer << infile.rdbuf();
+    string programcode = buffer.str();
+
+    vector<Token> ts = tokenize_regex(programcode);
+    print_all_tokens(ts);
+
+    return 0;
+}
--- tokens.h
+++ tokens.h
@@ -77,5 +77,67 @@ struct Token {
 
         }
 };
+string fromTokenTypeToStringGo(TokenType type)
+{
+    switch(type)
+    {
+        case TokenType::T_FUNCTION: return "T_FUNCTION";
+        case TokenType::T_INT: return "T_INT";
+        case TokenType::T_FLOAT: return "T_FLOAT";
+        case TokenType::T_STRING: return "T_STRING";
+        case TokenType::T_BOOL: return "T_BOOL";
+        case TokenType::T_RETURN: return "T_RETURN";
+        case TokenType::T_IF: return "T_IF";
+        case TokenType::T_ELSE: return "T_ELSE";
+        case TokenType::T_WHILE: return "T_WHILE";
+        case TokenType::T_FOR: return "T_FOR";
+
+        case TokenType::T_IDENTIFIER: return "T_IDENTIFIER";
+        case TokenType::T_INTLIT: return "T_INTLIT";
+        case TokenType::T_FLOATLIT: return "T_FLOATLIT";
+        case TokenType::T_STRINGLIT: return "T_STRINGLIT";
+        case TokenType::T_BOOLLIT: return "T_BOOLLIT";
+
+        case TokenType::T_ASSIGN: return "T_ASSIGN";
+        case TokenType::T_EQUALSOP: return "T_EQUALSOP";
+        case TokenType::T_NOTEQUAL: return "T_NOTEQUAL";
+        case TokenType::T_LESS: return "T_LESS";
+        case TokenType::T_LESSEQ: return "T_LESSEQ";
+        case TokenType::T_GREATER: return "T_GREATER";
+        case TokenType::T_GREATEREQ: return "T_GREATEREQ";
+        case TokenType::T_PLUS: return "T_PLUS";
+        case TokenType::T_MINUS: return "T_MINUS";
+        case TokenType::T_MULT: return "T_MULT";
+        case TokenType::T_DIV: return "T_DIV";
+        case TokenType::T_MOD: return "T_MOD";
+        case TokenType::T_PLUSASSIGN: return "T_PLUSASSIGN";
+        case TokenType::T_MINUSASSIGN: return "T_MINUSASSIGN";
+        case TokenType::T_AND: return "T_AND";
+        case TokenType::T_OR: return "T_OR";
+        case TokenType::T_NOT: return "T_NOT";
+        case TokenType::T_BITAND: return "T_BITAND";
+        case TokenType::T_BITOR: return "T_BITOR";
+        case TokenType::T_BITXOR: return "T_BITXOR";
+        case TokenType::T_BITNOT: return "T_BITNOT";
+        case TokenType::T_LSHIFT: return "T_LSHIFT";
+        case TokenType::T_RSHIFT: return "T_RSHIFT";
+
+        case TokenType::T_PARENL: return "T_PARENL";
+        case TokenType::T_PARENR: return "T_PARENR";
+        case TokenType::T_BRACEL: return "T_BRACEL";
+        case TokenType::T_BRACER: return "T_BRACER";
+        case TokenType::T_BRACKETL: return "T_BRACKETL";
+        case TokenType::T_BRACKETR: return "T_BRACKETR";
+        case TokenType::T_COMMA: return "T_COMMA";
+        case TokenType::T_SEMICOLON: return "T_SEMICOLON";
+        case TokenType::T_QUOTE: return "T_QUOTE";
+
+        case TokenType::T_COMMENT: return "T_COMMENT";
+        case TokenType::T_UNKNOWN: return "T_UNKNOWN";
+        case TokenType::T_EOF: return "T_EOF";
+        default: return "T_UNKNOWN";
+        }
+}
+
 
 #endif
