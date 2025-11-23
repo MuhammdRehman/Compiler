5d72e7b8899d1d92ca650d87b94385603c3cc4d8
Muhammad Rehman <ammuhammadrehman@gmail.com>
2025-11-23 17:07:57 +0500

    Add for and while loop handling in TAC

--- IR_Generation.cpp
+++ IR_Generation.cpp
@@ -1,3 +1,4 @@
+// irgen.cpp
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,12 +13,6 @@ using namespace std;
 struct IrGenException : public runtime_error {
     string msg;
     IrGenException(const string &m) : runtime_error(m), msg(m) {}
-};
-
-struct Operand {
-    string text;
-    Operand() : text("") {}
-    Operand(const string &t) : text(t) {}
 };
 
 enum class Opcode {
@@ -44,62 +39,39 @@ struct Instruction {
         : op(o), dst(""), left(""), right(""), extra("") {}
 };
 
-static string opcodeName(Opcode o) {
-    switch (o) {
-        case Opcode::ASSIGN: return "ASSIGN";
-        case Opcode::BINOP: return "BINOP";
-        case Opcode::LABEL: return "LABEL";
-        case Opcode::GOTO: return "GOTO";
-        case Opcode::IFGOTO: return "IFGOTO";
-        case Opcode::PARAM: return "PARAM";
-        case Opcode::CALL: return "CALL";
-        case Opcode::RETURN: return "RETURN";
-        case Opcode::FUNC_BEGIN: return "FUNC_BEGIN";
-        case Opcode::FUNC_END: return "FUNC_END";
-    }
-    return "OP";
-}
-
 struct IRModule {
     vector<Instruction> instrs;
 
     void push(const Instruction &i) { instrs.push_back(i); }
 
     void print(ostream &out = cout) const {
-        for (int i = 0; i < (int)instrs.size(); i++) {
+        for (int i = 0; i < (int)instrs.size(); ++i) {
             const Instruction &ins = instrs[i];
             if (ins.op == Opcode::LABEL) {
                 out << ins.left << ":\n";
-            }
-            else if (ins.op == Opcode::FUNC_BEGIN) {
+            } else if (ins.op == Opcode::FUNC_BEGIN) {
                 out << "func " << ins.extra << ":\n";
-            }
-            else if (ins.op == Opcode::FUNC_END) {
+            } else if (ins.op == Opcode::FUNC_END) {
                 out << "endfunc " << ins.extra << "\n";
-            }
-            else if (ins.op == Opcode::ASSIGN) {
+            } else if (ins.op == Opcode::ASSIGN) {
                 out << "  " << ins.dst << " = " << ins.left << "\n";
-            }
-            else if (ins.op == Opcode::BINOP) {
+            } else if (ins.op == Opcode::BINOP) {
                 out << "  " << ins.dst << " = " << ins.left
                     << " " << ins.extra << " " << ins.right << "\n";
-            }
-            else if (ins.op == Opcode::GOTO) {
+            } else if (ins.op == Opcode::GOTO) {
                 out << "  goto " << ins.left << "\n";
-            }
-            else if (ins.op == Opcode::IFGOTO) {
+            } else if (ins.op == Opcode::IFGOTO) {
                 out << "  if " << ins.left << " goto " << ins.right << "\n";
-            }
-            else if (ins.op == Opcode::PARAM) {
+            } else if (ins.op == Opcode::PARAM) {
                 out << "  param " << ins.left << "\n";
-            }
-            else if (ins.op == Opcode::CALL) {
+            } else if (ins.op == Opcode::CALL) {
                 out << "  " << ins.dst << " = call "
                     << ins.extra << ", " << ins.left << "\n";
-            }
-            else if (ins.op == Opcode::RETURN) {
-                if (ins.left == "") out << "  return\n";
+            } else if (ins.op == Opcode::RETURN) {
+                if (ins.left.empty()) out << "  return\n";
                 else out << "  return " << ins.left << "\n";
+            } else {
+                out << "  <unknown instr>\n";
             }
         }
     }
@@ -110,12 +82,13 @@ class IRGenerator {
     int tempCounter;
     int labelCounter;
     map<string,string> currentFunctionLocals;
+    vector<string> breakTargets;
 
 public:
     IRGenerator() : tempCounter(0), labelCounter(0) {}
 
     IRModule generate(Program &prog) {
-        for (int i = 0; i < (int)prog.funcs.size(); i++) {
+        for (int i = 0; i < (int)prog.funcs.size(); ++i) {
             shared_ptr<FunctionDecl> f = prog.funcs[i];
             generateFunction(f);
         }
@@ -124,12 +97,12 @@ public:
 
 private:
     string newTemp() {
-        tempCounter++;
+        tempCounter = tempCounter + 1;
         return string("t") + to_string(tempCounter);
     }
 
     string newLabel(const string &prefix) {
-        labelCounter++;
+        labelCounter = labelCounter + 1;
         return prefix + to_string(labelCounter);
     }
 
@@ -140,7 +113,7 @@ private:
         module.push(ib);
 
         currentFunctionLocals.clear();
-        for (int p = 0; p < (int)f->params.size(); p++) {
+        for (int p = 0; p < (int)f->params.size(); ++p) {
             string pname = f->params[p].ident;
             currentFunctionLocals[pname] = pname;
         }
@@ -158,8 +131,9 @@ private:
 
         BlockStmt* bs = dynamic_cast<BlockStmt*>(s.get());
         if (bs != NULL) {
-            for (int i = 0; i < (int)bs->stmts.size(); i++)
+            for (int i = 0; i < (int)bs->stmts.size(); ++i) {
                 generateStmt(bs->stmts[i]);
+            }
             return;
         }
 
@@ -167,7 +141,6 @@ private:
         if (vd != NULL) {
             string rhs = generateExpr(vd->init);
             currentFunctionLocals[vd->ident] = vd->ident;
-
             Instruction i;
             i.op = Opcode::ASSIGN;
             i.dst = vd->ident;
@@ -191,8 +164,9 @@ private:
         if (ret != NULL) {
             Instruction i;
             i.op = Opcode::RETURN;
-            if (ret->value != NULL)
+            if (ret->value != NULL) {
                 i.left = generateExpr(ret->value);
+            }
             module.push(i);
             return;
         }
@@ -205,10 +179,11 @@ private:
 
         IfStmt* ifs = dynamic_cast<IfStmt*>(s.get());
         if (ifs != NULL) {
+            string labelThen = newLabel("L_if_then_");
+            string labelElse = newLabel("L_if_else_");
+            string labelEnd = newLabel("L_if_end_");
+
             string condTemp = generateExpr(ifs->cond);
-            string labelThen = newLabel("L_then_");
-            string labelElse = newLabel("L_else_");
-            string labelEnd = newLabel("L_end_");
 
             Instruction ic;
             ic.op = Opcode::IFGOTO;
@@ -216,73 +191,50 @@ private:
             ic.right = labelThen;
             module.push(ic);
 
+            Instruction ig;
+            ig.op = Opcode::GOTO;
+            ig.left = labelElse;
+            module.push(ig);
+
+            Instruction il;
+            il.op = Opcode::LABEL;
+            il.left = labelThen;
+            module.push(il);
+
+            generateStmt(ifs->thenBody);
+
+            Instruction ig2;
+            ig2.op = Opcode::GOTO;
+            ig2.left = labelEnd;
+            module.push(ig2);
+
+            Instruction il2;
+            il2.op = Opcode::LABEL;
+            il2.left = labelElse;
+            module.push(il2);
+
             if (ifs->elseBody != NULL) {
-                Instruction ig;
-                ig.op = Opcode::GOTO;
-                ig.left = labelElse;
-                module.push(ig);
-
-                Instruction il;
-                il.op = Opcode::LABEL;
-                il.left = labelThen;
-                module.push(il);
-
-                generateStmt(ifs->thenBody);
-
-                Instruction ig2;
-                ig2.op = Opcode::GOTO;
-                ig2.left = labelEnd;
-                module.push(ig2);
-
-                Instruction il2;
-                il2.op = Opcode::LABEL;
-                il2.left = labelElse;
-                module.push(il2);
-
                 generateStmt(ifs->elseBody);
-
-                Instruction il3;
-                il3.op = Opcode::LABEL;
-                il3.left = labelEnd;
-                module.push(il3);
-            } else {
-                Instruction il;
-                il.op = Opcode::LABEL;
-                il.left = labelThen;
-                module.push(il);
-
-                generateStmt(ifs->thenBody);
-
-                Instruction il2;
-                il2.op = Opcode::LABEL;
-                il2.left = labelEnd;
-                module.push(il2);
             }
+
+            Instruction il3;
+            il3.op = Opcode::LABEL;
+            il3.left = labelEnd;
+            module.push(il3);
+
             return;
         }
 
         WhileStmt* ws = dynamic_cast<WhileStmt*>(s.get());
         if (ws != NULL) {
-            string labelCond = newLabel("L_while_cond_");
+            string labelStart = newLabel("L_while_start_");
             string labelBody = newLabel("L_while_body_");
             string labelEnd = newLabel("L_while_end_");
 
-            Instruction igoto;
-            igoto.op = Opcode::GOTO;
-            igoto.left = labelCond;
-            module.push(igoto);
-
-            Instruction ibody;
-            ibody.op = Opcode::LABEL;
-            ibody.left = labelBody;
-            module.push(ibody);
-
-            generateStmt(ws->body);
-
-            Instruction icond;
-            icond.op = Opcode::LABEL;
-            icond.left = labelCond;
-            module.push(icond);
+            Instruction ilstart;
+            ilstart.op = Opcode::LABEL;
+            ilstart.left = labelStart;
+            module.push(ilstart);
 
             string condTemp = generateExpr(ws->cond);
 
@@ -292,40 +244,49 @@ private:
             ifg.right = labelBody;
             module.push(ifg);
 
-            Instruction iend;
-            iend.op = Opcode::LABEL;
-            iend.left = labelEnd;
-            module.push(iend);
-
-            return;
-        }
-
-        ForStmt* fs = dynamic_cast<ForStmt*>(s.get());
-        if (fs != NULL) {
-            string labelCond = newLabel("L_for_cond_");
-            string labelBody = newLabel("L_for_body_");
-            string labelEnd = newLabel("L_for_end_");
-
-            if (fs->init != NULL) generateStmt(fs->init);
-
-            Instruction ig;
-            ig.op = Opcode::GOTO;
-            ig.left = labelCond;
-            module.push(ig);
+            Instruction gotoEnd;
+            gotoEnd.op = Opcode::GOTO;
+            gotoEnd.left = labelEnd;
+            module.push(gotoEnd);
 
             Instruction ilbody;
             ilbody.op = Opcode::LABEL;
             ilbody.left = labelBody;
             module.push(ilbody);
 
-            generateStmt(fs->body);
+            breakTargets.push_back(labelEnd);
 
-            if (fs->update != NULL) generateStmt(fs->update);
+            generateStmt(ws->body);
 
-            Instruction ilcond;
-            ilcond.op = Opcode::LABEL;
-            ilcond.left = labelCond;
-            module.push(ilcond);
+            breakTargets.pop_back();
+
+            Instruction igotoStart;
+            igotoStart.op = Opcode::GOTO;
+            igotoStart.left = labelStart;
+            module.push(igotoStart);
+
+            Instruction ilend;
+            ilend.op = Opcode::LABEL;
+            ilend.left = labelEnd;
+            module.push(ilend);
+
+            return;
+        }
+
+        ForStmt* fs = dynamic_cast<ForStmt*>(s.get());
+        if (fs != NULL) {
+            string labelStart = newLabel("L_for_start_");
+            string labelBody = newLabel("L_for_body_");
+            string labelEnd = newLabel("L_for_end_");
+
+            if (fs->init != NULL) {
+                generateStmt(fs->init);
+            }
+
+            Instruction ilstart;
+            ilstart.op = Opcode::LABEL;
+            ilstart.left = labelStart;
+            module.push(ilstart);
 
             if (fs->condition != NULL) {
                 string condTemp = generateExpr(fs->condition);
@@ -334,18 +295,56 @@ private:
                 ifg.left = condTemp;
                 ifg.right = labelBody;
                 module.push(ifg);
+
+                Instruction gotoEnd;
+                gotoEnd.op = Opcode::GOTO;
+                gotoEnd.left = labelEnd;
+                module.push(gotoEnd);
             } else {
-                Instruction ifg;
-                ifg.op = Opcode::GOTO;
-                ifg.left = labelBody;
-                module.push(ifg);
+                Instruction gotoBody;
+                gotoBody.op = Opcode::GOTO;
+                gotoBody.left = labelBody;
+                module.push(gotoBody);
             }
 
-            Instruction iend;
-            iend.op = Opcode::LABEL;
-            iend.left = labelEnd;
-            module.push(iend);
+            Instruction ilbody;
+            ilbody.op = Opcode::LABEL;
+            ilbody.left = labelBody;
+            module.push(ilbody);
 
+            breakTargets.push_back(labelEnd);
+
+            generateStmt(fs->body);
+
+            if (fs->update != NULL) {
+                generateStmt(fs->update);
+            }
+
+            breakTargets.pop_back();
+
+            Instruction igotoStart;
+            igotoStart.op = Opcode::GOTO;
+            igotoStart.left = labelStart;
+            module.push(igotoStart);
+
+            Instruction ilend;
+            ilend.op = Opcode::LABEL;
+            ilend.left = labelEnd;
+            module.push(ilend);
+
+            return;
+        }
+
+        BreakStmt* br = dynamic_cast<BreakStmt*>(s.get());
+        if (br != NULL) {
+            if ((int)breakTargets.size() == 0) {
+                throw IrGenException("break used outside loop");
+            }
+            string target = breakTargets[(int)breakTargets.size() - 1];
+            Instruction ig;
+            ig.op = Opcode::GOTO;
+            ig.left = target;
+            module.push(ig);
             return;
         }
 
@@ -375,7 +374,6 @@ private:
             string leftOp = generateExpr(be->left);
             string rightOp = generateExpr(be->right);
             string temp = newTemp();
-
             Instruction i;
             i.op = Opcode::BINOP;
             i.dst = temp;
@@ -383,14 +381,13 @@ private:
             i.right = rightOp;
             i.extra = be->op;
             module.push(i);
-
             return temp;
         }
 
         CallExpr* ce = dynamic_cast<CallExpr*>(e.get());
         if (ce != NULL) {
             int argc = (int)ce->args.size();
-            for (int i = 0; i < argc; i++) {
+            for (int i = 0; i < argc; ++i) {
                 string a = generateExpr(ce->args[i]);
                 Instruction ip;
                 ip.op = Opcode::PARAM;
@@ -398,10 +395,8 @@ private:
                 module.push(ip);
             }
 
-            IdentifierExpr* calleeId =
-                dynamic_cast<IdentifierExpr*>(ce->callee.get());
-            if (calleeId == NULL)
-                throw IrGenException("Call target must be identifier");
+            IdentifierExpr* calleeId = dynamic_cast<IdentifierExpr*>(ce->callee.get());
+            if (calleeId == NULL) throw IrGenException("Call target must be identifier");
 
             string fname = calleeId->name;
             string dest = newTemp();
@@ -412,7 +407,6 @@ private:
             ic.left = to_string(argc);
             ic.extra = fname;
             module.push(ic);
-
             return dest;
         }
 
